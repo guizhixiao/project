aef764ed9324c5ca561c3dfba716afff252b04fb
guizhixiao <1002142758@qq.com>
2017-12-29 18:15:26 +0800

    最终修改Test

--- Test.java
+++ Test.java
@@ -2,7 +2,7 @@ public class Test{
 	public static void main(String[] args){
 		System.out.println("test23");
 		System.out.println("test2");
-		System.out.println("csss");//修改打印信息sss为csss
+		System.out.println("csss");//修改打印信息sss为csss !!!
 		System.out.println("ddd");
 	}
 }
