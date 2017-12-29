6f7500df848559e75fefbf11dc1b5df0c27b20df
guizhixiao <1002142758@qq.com>
2017-12-29 17:47:52 +0800

    修改Test信息

--- Test.java
+++ Test.java
@@ -2,7 +2,8 @@ public class Test{
 	public static void main(String[] args){
 		System.out.println("test23");
 		System.out.println("test2");
-		System.out.println("sss");
-		System.out.println("sss");
+		System.out.println("csss");
+		System.out.println("ddd");
+		// 在dev中修改打印信息
 	}
 }
