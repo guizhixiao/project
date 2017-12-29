eecfb0161ff10a74a3c48ab31f7c2bcfa03b22ca
guizhixiao <1002142758@qq.com>
2017-12-29 17:52:01 +0800

    新开始

--- Test.java
+++ Test.java
@@ -2,8 +2,7 @@ public class Test{
 	public static void main(String[] args){
 		System.out.println("test23");
 		System.out.println("test2");
-		System.out.println("csss");
+		System.out.println("sss");
 		System.out.println("ddd");
-		// 在dev中修改打印信息
 	}
 }
