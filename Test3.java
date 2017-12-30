2f4cc2c59a1bc7bf919e3b0e2566016ee588c1a5
guizhixiao <1002142758@qq.com>
2017-12-30 20:27:58 +0800

    addTest3

---
+++ Test3.java
@@ -0,0 +1,9 @@
+public class Test{
+	public static void main(String[] args){
+		System.out.println("test23");
+		System.out.println("test");//hotfix将打印信息test2变为test!!!
+		System.out.println("test2");
+		System.out.println("csss");//修改打印信息sss为csss !!!
+		System.out.println("ddd");
+	}
+}
